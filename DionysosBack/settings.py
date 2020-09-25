f07c067105e1f5ec57de077c5490af1799205d66
Paul-Antoine Cabrera <paulantoine.cabrera@gmail.com>
2020-09-25 22:52:28 +0200

    test user info

--- DionysosBack/settings.py
+++ DionysosBack/settings.py
@@ -60,7 +60,6 @@ AUTHENTICATION_BACKENDS = (
 'oauth2_provider.backends.OAuth2Backend',
 # Uncomment following if you want to access the admin
 #'django.contrib.auth.backends.ModelBackend'
-'...',
 )
 
 
