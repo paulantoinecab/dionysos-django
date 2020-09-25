c6f75dc37a3f7368c023319a92e927adb412bc3e
Paul-Antoine Cabrera <paulantoine.cabrera@gmail.com>
2020-09-25 22:36:03 +0200

    test user info

--- DionysosServer/urls.py
+++ DionysosServer/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('table/<uuid:table_id>/', views.sit_to_table, name='Sit to table'),
     path('order/', views.create_order, name='Create payment intent'),
     path('account/create', views.create_account, name='Create user account'),
-#    path('account/login', views.log_in, name='Log in'),
+    path('account/userinfo', views.get_user_info, name='User info'),
     path('account/logout', views.log_out, name='Log out'),
 ]
