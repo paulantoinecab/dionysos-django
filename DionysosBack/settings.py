d2e9e65a1bf08f6dad53745023b61f8ca2e8a116
Paul-Antoine Cabrera <paulantoine.cabrera@gmail.com>
2020-09-26 14:31:31 +0200

    oauth2

--- DionysosBack/settings.py
+++ DionysosBack/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 AUTHENTICATION_BACKENDS = (
 'oauth2_provider.backends.OAuth2Backend',
 # Uncomment following if you want to access the admin
-#'django.contrib.auth.backends.ModelBackend'
+'django.contrib.auth.backends.ModelBackend'
 )
 
 
